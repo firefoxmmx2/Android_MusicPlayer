5ff5ea12f38f5c3130f08eead06c8c4c5c059a15
firefoxmmx2 <firefoxmmx@gmail.com>
2013-10-25 00:30:49 +0800

    impl music player ui part

--- src/org/ffmmx/example/musicplayer/activity.scala
+++ src/org/ffmmx/example/musicplayer/activity.scala
@@ -9,50 +9,85 @@ class MainActivity extends SActivity {
   override def basis: SActivity = this
 
   implicit override val ctx: SActivity = basis
-
   onCreate({
     setContentView(R.layout.main)
 
     //播放暂停按钮
     val playPauseButton = find[ImageButton](R.id.playPauseButton)
       .onClick({
-        // TODO 发送播放或者停止请求到播放服务
+      // 发送播放或者停止请求到播放服务
+      sendBroadcast(
+        SIntent(Constants.MUSIC_SERVICE_ACTION)
+          .putExtra("action", Constants.PLAY_ACTION_PLAYPAUSE)
+          .putExtra("song", "")
+      )
     })
-      .onKey((v, id, event) => {
-      // TODO 播放暂停按钮图标状态改变
-      false
-    })
+
     //上一首按钮
     val previousButton = find[ImageButton](R.id.previousButton)
       .onClick({
-      // TODO 发送上一首请求到播放服务
+      // 发送上一首请求到播放服务
+      sendBroadcast(
+        SIntent(Constants.MUSIC_SERVICE_ACTION)
+          .putExtra("action", Constants.PLAY_ACTION_PREVIOUS)
+          .putExtra("song", "")
+
+      )
     })
-      .onKey((v,id,event) => {
-      // TODO  上一首按钮图标状态改变
-      false
-    })
+
     //下一首按钮
     val nextButton = find[ImageButton](R.id.nextButton)
       .onClick({
-      // TODO 发送下一首请求到播放服务
-    })
-      .onKey((v,id,event) => {
-      // TODO  下一首按钮图标状态改变
-      false
+      // 发送下一首请求到播放服务
+      sendBroadcast(
+        SIntent(Constants.MUSIC_SERVICE_ACTION)
+          .putExtra("action", Constants.PLAY_ACTION_NEXT)
+          .putExtra("song", "")
+      )
     })
 
     // TODO 播放列表
+    // 注册播放器广播
     broadcastReceiver(Constants.MUSIC_PLAYER_ACTION) {
       (context, intent) => {
-        // TODO 更新界面信息
+        // TODO 更新界面图标
+        intent.getIntExtra("playStatus",Constants.PLAY_STATUS_STOP) match {
+          case Constants.PLAY_STATUS_PAUSE =>
+            playPauseButton.imageResource(R.drawable.pause_sel)
+          case _ =>
+            playPauseButton.imageResource(R.drawable.play_sel)
+        }
+
+        // TODO 更新界面歌曲
+
+        // TODO 更新界面时间
+
       }
     }
+
+    //开始播放服务
+    startService(SIntent[MusicPlayService])
   })
 
 
 }
 
 object Constants {
+  //播放器广播action
   val MUSIC_PLAYER_ACTION = "org.ffmmx.example.musicplayer.MusicPlayerActivity"
+  //播放服务广播action
   val MUSIC_SERVICE_ACTION = "org.ffmmx.example.musicplayer.MusicPlayerService"
+
+  //播放状态 停止
+  val PLAY_STATUS_STOP = 0
+  //播放状态 播放
+  val PLAY_STATUS_PLAY = 1
+  //播放状态 暂停
+  val PLAY_STATUS_PAUSE = 2
+
+  //播放动作
+  val PLAY_ACTION_PLAYPAUSE = "playpause"
+  val PLAY_ACTION_STOP = "stop"
+  val PLAY_ACTION_PREVIOUS = "previous"
+  val PLAY_ACTION_NEXT = "next"
 }
--- src/org/ffmmx/example/musicplayer/service.scala
+++ src/org/ffmmx/example/musicplayer/service.scala
@@ -6,9 +6,28 @@ import android.os.IBinder
 
 
 class MusicPlayService extends SService{
+  var status = Constants.PLAY_STATUS_PAUSE
+
   def onBind(intent: Intent): IBinder = null
 
   override def onCreate(): Unit = {
     // TODO 播放服务实现
+
+    // 音乐服务广播
+    broadcastReceiver(Constants.MUSIC_SERVICE_ACTION) {
+      (context,intent) => {
+        intent.getStringExtra("action") match {
+          case Constants.PLAY_ACTION_PLAYPAUSE =>
+            // TODO 播放暂停实现
+          case Constants.PLAY_ACTION_STOP =>
+            // TODO 实现停止
+          case Constants.PLAY_ACTION_PREVIOUS =>
+            // TODO 实现上一首
+          case Constants.PLAY_ACTION_NEXT  =>
+            // TODO 实现下一首
+
+        }
+      }
+    }
   }
 }
